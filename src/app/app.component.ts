9995d3d7af00fdbc553c7d33d75b1326c3201d1e
peninah-njeri <peninahgathuru@gmail.com>
2019-09-23 15:04:26 +0300

    add a value.date in the submitForm function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,15 @@ import { Quote } from './models/quote';
 export class AppComponent {
   title = 'quotes';
   quotes: Quote[] = [
-    new Quote("The only way to keep your health is to eat what you don't want, drink what you don't like, and do what you'd rather not.","Mark Twain", "Abram"),
-    new Quote("To keep the body in good health is a duty, otherwise we shall not be able to keep our mind strong and clear.","Buddha", "James"),
-    new Quote("Those who do not find time for exercise will have to find time for illness.","Edward Smith-Stanley", "Pete")
+
+    new Quote("The only way to keep your health is to eat what you don't want, drink what you don't like, and do what you'd rather not.","Mark Twain", "Abram", new Date(2019,9,23)),
+    new Quote("To keep the body in good health is a duty, otherwise we shall not be able to keep our mind strong and clear.","Buddha", "James", new Date(2019,9,23)),
+    new Quote("Those who do not find time for exercise will have to find time for illness.","Edward Smith-Stanley", "Pete", new Date(2019,9,23))
   ];
 
 
   submitForm(value){
-    const quote = new Quote(value.quote, value.author, value.publisher)
+    const quote = new Quote(value.quote, value.author, value.publisher, value.dateAgo)
     this.quotes.push(quote);
     console.log(quote)
   }
