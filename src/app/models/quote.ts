cba21c57e087a7a088de75062a3b79908cd609a3
peninah-njeri <peninahgathuru@gmail.com>
2019-09-23 15:20:26 +0300

    add a dateAgo in the constructor at the class Quote blueprint

--- src/app/models/quote.ts
+++ src/app/models/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     upvotes;
     downvotes;
-    constructor(public body:string, public author: string, public publisher: string){
+    constructor(public body:string, public author: string, public publisher: string, public dateAgo : Date){
         this.upvotes = 0;
         this.downvotes = 0;
     }
